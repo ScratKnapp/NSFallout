d241f48e020ab88a9b70749264463a3ae4d19556
Scrat Knapp <hatdescript@gmail.com>
2026-02-12 17:44:01 -0600

    Thermal cells

---
+++ fallout/plugins/combat/plugins/fancyammo/items/ammofancy/sh_mfcthermal.lua
@@ -0,0 +1,15 @@
+ITEM.name = "Microfusion Cell - Thermal Charge"
+ITEM.desc = "A medium sized self-contained fusion plant for rifle-sized energy weapons. This one provides a prolonged output that often causes burns."
+ITEM.uniqueID = "ammo_mfcthermal"
+ITEM.model = "models/fallout new vegas/microfusion_cell.mdl"
+ITEM.category = "Ammunition"
+
+ITEM.ammo = "Microfusion Cell"
+ITEM.ammoAmount = 30 --single bullet
+ITEM.bullet = "Microfusion Cell Thermal Charge"
+ITEM.maxstack = 100
+
+ITEM.price = 40
+ITEM.permit = "permit_general"
+
+ITEM.dmgT = "Microfusion Cell Thermal Charge"
