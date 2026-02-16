716d8abf13a0a575c0d98ec777bbe4aeee10f17e
Scrat Knapp <hatdescript@gmail.com>
2026-02-15 22:03:16 -0600

    Remove base NS from repo

--- nutscript/plugins/notices/sh_plugin.lua
+++
@@ -1,5 +0,0 @@
-PLUGIN.name = "Notices"
-PLUGIN.author = "Cheesenut"
-PLUGIN.desc = "Adds a panel for notifications."
-
-nut.util.include("cl_notice.lua")
--- nutscript/plugins/nstheme/sh_plugin.lua
+++
@@ -1,9 +0,0 @@
-PLUGIN.name = "NutScript Theme"
-PLUGIN.author = "Cheesenut"
-PLUGIN.desc = "Adds a dark Derma skin for NutScript."
-
-if (CLIENT) then
-	function PLUGIN:ForceDermaSkin()
-		return "nutscript"
-	end
-end
