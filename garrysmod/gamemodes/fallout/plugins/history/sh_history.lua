95a9a70997e37737821db2a5d88fa7edbac3f21e
Scrat Knapp <hatdescript@gmail.com>
2026-05-30 16:31:29 -0500

    Adjust history descs and boosts to accurately reflect what you get

--- garrysmod/gamemodes/fallout/plugins/history/sh_history.lua
+++ garrysmod/gamemodes/fallout/plugins/history/sh_history.lua
@@ -3,7 +3,7 @@ local PLUGIN = PLUGIN
 local HISTORY = {}
 HISTORY.uid = "military" 
 HISTORY.name = "Ex-Military"
-HISTORY.desc = "At one point, you fought for a cause. Now you've left that life, but your training yet remains.\n+1 Strength, +4 Athletics.\nYou are not required to roleplay this background if you select it."
+HISTORY.desc = "At one point, you fought for a cause. Now you've left that life, but your training yet remains.\n+1 Strength, +5 Explosives.\nYou are not required to roleplay this background if you select it."
 HISTORY.category = "History"
 
 HISTORY.func = function(client, character)
@@ -22,7 +22,7 @@ HISTORIES:Register(HISTORY)
 local HISTORY = {}
 HISTORY.uid = "prospector" 
 HISTORY.name = "Prospector"
-HISTORY.desc = "You scoured the wastes in search of goods and salvage, and you've trained your eye to spot trouble before it spots you.\n+1 Perception, +4 Sneak.\nYou are not required to roleplay this background if you select it."
+HISTORY.desc = "You scoured the wastes in search of goods and salvage, and you've trained your eye to spot trouble before it spots you.\n+1 Perception, +10 Sneak.\nYou are not required to roleplay this background if you select it."
 HISTORY.category = "History"
 --[[
 HISTORY.items = {
@@ -40,7 +40,7 @@ HISTORIES:Register(HISTORY)
 local HISTORY = {}
 HISTORY.uid = "tribal" 
 HISTORY.name = "Tribal"
-HISTORY.desc = "You once belonged to a tribe, upholding customs and tradition. Although you are no longer with them, their beliefs still guide you.\n+1 Endurance, +2 Throwing.\nYou are not required to roleplay this background if you select it."
+HISTORY.desc = "You once belonged to a tribe, upholding customs and tradition. Although you are no longer with them, their beliefs still guide you.\n+1 Endurance, +5 Throwing.\nYou are not required to roleplay this background if you select it."
 HISTORY.category = "History"
 --[[
 HISTORY.items = {
@@ -58,7 +58,7 @@ HISTORIES:Register(HISTORY)
 local HISTORY = {}
 HISTORY.uid = "caravan" 
 HISTORY.name = "Former Merchant"
-HISTORY.desc = "You know how to make a deal, a sharp tongue and quick wit have helped you trade and run caravans in the past.\n+1 Charisma, +4 Speech.\nYou are not required to roleplay this background if you select it."
+HISTORY.desc = "You know how to make a deal, a sharp tongue and quick wit have helped you trade and run caravans in the past.\n+1 Charisma, +10 Speech.\nYou are not required to roleplay this background if you select it."
 HISTORY.category = "History"
 HISTORY.func = function(client, character)
 	character:setData("history", HISTORY.uid)
@@ -72,7 +72,7 @@ HISTORIES:Register(HISTORY)
 local HISTORY = {}
 HISTORY.uid = "doctor" 
 HISTORY.name = "Medic"
-HISTORY.desc = "You've spent a long time learning how to patch people up and keep them in one piece.\n+1 Intelligence, +4 Medicine.\nYou are not required to roleplay this background if you select it."
+HISTORY.desc = "You've spent a long time learning how to patch people up and keep them in one piece.\n+1 Intelligence, +10 Medicine.\nYou are not required to roleplay this background if you select it."
 HISTORY.category = "History"
 --[[
 HISTORY.items = {
@@ -90,7 +90,7 @@ HISTORIES:Register(HISTORY)
 local HISTORY = {}
 HISTORY.uid = "exncr" 
 HISTORY.name = "Former Courier"
-HISTORY.desc = "You once held the job of delivering through the wastes, and have learned how to move and avoid danger.\n+1 Agility, +2 Evasion.\nYou are not required to roleplay this background if you select it."
+HISTORY.desc = "You once held the job of delivering through the wastes, and have learned how to move and avoid danger.\n+1 Agility, +10 Athletics.\nYou are not required to roleplay this background if you select it."
 HISTORY.category = "History"
 --[[
 HISTORY.items = {
@@ -108,7 +108,7 @@ HISTORIES:Register(HISTORY)
 local HISTORY = {}
 HISTORY.uid = "exraider" 
 HISTORY.name = "Ex-Raider"
-HISTORY.desc = "Despite being high out of your mind, or just sick on the head, you've survived through brutality and chance.\n+1 Luck, +4 Lockpicking.\nYou are not required to roleplay this background if you select it."
+HISTORY.desc = "Despite being high out of your mind, or just sick on the head, you've survived through brutality and chance.\n+1 Luck, +10 Lockpicking.\nYou are not required to roleplay this background if you select it."
 HISTORY.category = "History"
 --[[
 HISTORY.items = {
@@ -139,7 +139,7 @@ HISTORY.items = {
 HISTORY.func = function(client, character)
 	character:setData("history", HISTORY.uid)
     client:giveTrait("gunsmith")
-	character:updateSkill("repair", 25)
+	character:updateSkill("repair", 20)
 end
 HISTORIES:Register(HISTORY)
 
@@ -158,7 +158,7 @@ HISTORY.items = {
 HISTORY.func = function(client, character)
 	character:setData("history", HISTORY.uid)
     client:giveTrait("science")
-	character:updateSkill("science", 25)
+	character:updateSkill("science", 20)
 end
 HISTORIES:Register(HISTORY)
 
@@ -177,6 +177,6 @@ HISTORY.items = {
 HISTORY.func = function(client, character)
 	character:setData("history", HISTORY.uid)
     client:giveTrait("chef")
-	character:updateSkill("survival", 25)
+	character:updateSkill("survival", 20)
 end
 HISTORIES:Register(HISTORY)
