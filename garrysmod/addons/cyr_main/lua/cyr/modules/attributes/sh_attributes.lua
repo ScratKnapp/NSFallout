e334963011ddbf350511cfc440bb9a4a0544a480
Evie <evie.melinoe.wrynn@gmail.com>
2026-06-03 14:23:14 +0100

    Removed Custom Attribute Loading

--- garrysmod/addons/cyr_main/lua/cyr/modules/attributes/sh_attributes.lua
+++ garrysmod/addons/cyr_main/lua/cyr/modules/attributes/sh_attributes.lua
@@ -1,10 +1,1 @@
--- CYR Attributes Module
--- Loads custom intelligence and charisma attributes
-
--- Wait for nutscript to be initialized before loading attributes
-hook.Add("InitializedPlugins", "CYR_LoadAttributes", function()
-    if nut and nut.attribs and nut.attribs.loadFromDir then
-        nut.attribs.loadFromDir("cyr/modules/attributes")
-        print("[CYR] Custom attributes (Intelligence, Charisma) loaded!")
-    end
-end)
+-- EMPTY FILE KEEPING FOR SAKE OF INCLUDE SAKE
